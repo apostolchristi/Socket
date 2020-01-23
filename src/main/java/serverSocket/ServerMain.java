0ad0b179f791a21720007df9ba6ad1b98fa530a5
Apostol Cristian <Cristyapostol93@gmail.com>
2020-01-23 21:03:05 +0200

    Feature/server socket (#5)
    * feat(socketServer) add socket server
    
    * feat(clinetSystemInfo) add client systeminfo

---
+++ src/main/java/serverSocket/ServerMain.java
@@ -0,0 +1,12 @@
+package serverSocket;
+
+import java.io.IOException;
+
+public class ServerMain {
+
+  public static void main(String[] args) throws IOException {
+
+	ThreadedEchoServer server = new ThreadedEchoServer();
+	server.start(9122);
+  }
+}
